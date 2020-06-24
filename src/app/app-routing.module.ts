03376c08e7d4e8a1d6222f025659a759eb50c21c
rsouza <rafael.souza.fijalkowski@denkwerk.com>
2020-06-24 10:08:41 +0200

    Initial Setup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {CompanyListComponent} from './company/company-list/company-list.component';
+import {CompanyAddComponent} from './company/company-add/company-add.component';
+import {CompanyEditComponent} from './company/company-edit/company-edit.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'company', component: CompanyListComponent },
+  { path: 'company/:id', component: CompanyEditComponent },
+  { path: 'company/add', component: CompanyAddComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
