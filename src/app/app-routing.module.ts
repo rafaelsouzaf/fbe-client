71e61553fc688cf024582da2038b16d8195931fa
rsouza <rafael.souza.fijalkowski@denkwerk.com>
2020-06-26 10:47:04 +0200

    Fix edit route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {CompanyListComponent} from './company/company-list/company-list.component';
-import {CompanyAddComponent} from './company/company-add/company-add.component';
-import {CompanyEditComponent} from './company/company-edit/company-edit.component';
+import { HomeComponent } from './home/home.component';
+import { CompanyListComponent } from './company/company-list/company-list.component';
+import { CompanyAddComponent } from './company/company-add/company-add.component';
+import { CompanyEditComponent } from './company/company-edit/company-edit.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'company', component: CompanyListComponent },
-  { path: 'company/:id', component: CompanyEditComponent },
-  { path: 'company/add', component: CompanyAddComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'company', component: CompanyListComponent },
+    { path: 'company/edit/:id', component: CompanyEditComponent },
+    { path: 'company/add', component: CompanyAddComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
