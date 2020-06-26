89e79e3c2f44f9d5b3cdd00fb49b0d9d9ad651a0
rsouza <rafael.souza.fijalkowski@denkwerk.com>
2020-06-26 10:50:31 +0200

    Prettier automatic format.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { CompanyListComponent } from './company/company-list/company-list.component';
 import { CompanyAddComponent } from './company/company-add/company-add.component';
 import { CompanyEditComponent } from './company/company-edit/company-edit.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    CompanyListComponent,
-    CompanyAddComponent,
-    CompanyEditComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        CompanyListComponent,
+        CompanyAddComponent,
+        CompanyEditComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+        ReactiveFormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
