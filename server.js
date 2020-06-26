c46cd55c65184a55b8f57217a61dbdb857187d56
rsouza <rafael.souza.fijalkowski@denkwerk.com>
2020-06-27 01:28:06 +0200

    Add heroku postbuild postinstall script

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ ngApp.use(express.static("./dist/fbe-client"));
 ngApp.get("/*", function(request, response) {
     response.sendFile(path.join(__dirname, "/dist/fbe-client/index.html"));
 });
-ngApp.listen(process.env.PORT || 8082);
+ngApp.listen(process.env.PORT || 8080);
