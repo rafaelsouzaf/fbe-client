98f0b852da87e8c62b7e5a0b28c19d6b9f2388b2
rsouza <rafael.souza.fijalkowski@denkwerk.com>
2020-06-26 17:31:45 +0200

    Add employee routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { HomeComponent } from './home/home.component';
 import { CompanyListComponent } from './company/company-list/company-list.component';
 import { CompanyAddComponent } from './company/company-add/company-add.component';
 import { CompanyEditComponent } from './company/company-edit/company-edit.component';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { EmployeeEditComponent } from './employee/employee-edit/employee-edit.component';
+import { EmployeeAddComponent } from './employee/employee-add/employee-add.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'company', component: CompanyListComponent },
     { path: 'company/edit/:id', component: CompanyEditComponent },
-    { path: 'company/add', component: CompanyAddComponent }
+    { path: 'company/add', component: CompanyAddComponent },
+    { path: 'employee', component: EmployeeListComponent },
+    { path: 'employee/edit/:id', component: EmployeeEditComponent },
+    { path: 'employee/add', component: EmployeeAddComponent }
 ];
 
 @NgModule({
